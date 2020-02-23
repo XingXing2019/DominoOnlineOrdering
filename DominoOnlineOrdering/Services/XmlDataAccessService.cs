700e447f45efe834eaeead55e0e61e4e80e2cdd3
Xing <x.xing.unsw@gmail.com>
2020-02-23 16:19:01 +1100

    Remove Unused library

--- DominoOnlineOrdering/Services/XmlDataAccessService.cs
+++ DominoOnlineOrdering/Services/XmlDataAccessService.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Xml.Serialization;
 using System.IO;
-using System.Xml;
-using DominoOnlineOrdering.Models;
-using System.Text;
-using System.Xml.Linq;
 
 namespace DominoOnlineOrdering.Services
 {
--- DominoOnlineOrdering/ViewModels/MainViewModel.cs
+++ DominoOnlineOrdering/ViewModels/MainViewModel.cs
@@ -8,7 +8,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows;
-using System.Windows.Input;
 
 namespace DominoOnlineOrdering.ViewModel
 {
